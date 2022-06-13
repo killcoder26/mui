f1318dea54689765a0f090eb813248a33c1728ab
killcoder26 <1928081@kiit.ac.in>
2022-06-13 12:55:06 +0530

    final styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,6 @@ import 'animate.css';
 import IconButton from '@mui/material/IconButton';
 import { RiHomeSmileLine } from "react-icons/ri";
 import Clock from 'react-live-clock';
-import { GrSun } from "react-icons/gr";
 import { IoPartlySunnyOutline } from "react-icons/io5";
 import { IoMoonOutline } from "react-icons/io5";
 
@@ -31,7 +30,7 @@ function Navbar() {
               fontSize: 32, color: "orangered"
             }} id="animated" >Everyone</Typography>
           </Typography>
-          <div className="greet">{time >= 12 ? time >= 16 ? <div>Good Evening <IoMoonOutline /></div> : <div>Good AfterNoon <GrSun /></div> : <div>Good Morning <IoPartlySunnyOutline /></div>}</div>
+          <div className="greet">{time >= 12 ? time >= 16 ? <div>Good Evening <IoMoonOutline /></div> : <div>Good AfterNoon </div> : <div>Good Morning <IoPartlySunnyOutline /></div>}</div>
           <div id='time'><Typography sx={{ color: "orangered", fontSize: 22, fontFamily: "Comic Sans MS" }} ><Clock format={'HH:mm:ss'} ticking={true} timezone={'Asia/Kolkata'} id="time" /></Typography></div>
         </Toolbar>
       </AppBar>
